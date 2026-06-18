5e0e075aaae59ce8a3fa74a84a235f57c3c97c5a
Robin Grass <hej@carbin.se>
2026-06-18 08:22:14 +0200

    chore: update scores

--- src/data.ts
+++ src/data.ts
@@ -26,6 +26,7 @@ export const leaderboard: Player[] = [
     scores: [
       { date: "2026-06-16", score: 938 },
       { date: "2026-06-17", score: 942 },
+      { date: "2026-06-18", score: 955 },
     ],
   },
   {
